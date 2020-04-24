76cf0528ef3d87973c680fa592a3b469dcce7fb2
th-of <thof@nmbu.no>
2020-04-24 12:55:58 +0200

    Calculates average distance between water molecules (oxygen point coordinate)

---
+++ WaterDens.R
@@ -0,0 +1,26 @@
+x <- readLines("lastframe.pdb")
+
+waters <- x[grepl("O   WAT", x) == TRUE]
+
+watcord <- regmatches(waters, gregexpr("(+|-)\\d+\\.\\d{3}", waters))
+
+watcord <- lapply(watcord, as.numeric)
+
+watcord <- as.data.frame(watcord, stringsAsFactors = FALSE)
+
+watcord <- as.data.frame(t(watcord))
+
+distance <- function(x1,x2,y1,y2,z1,z2){
+  return(sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2))
+}
+
+row.names(watcord) <- NULL
+
+distances <- c()
+
+for (i in 1:length(watcord$V1)){
+  distances2 <- sqrt((watcord$V1[i]-watcord$V1)**2 + (watcord$V2[i]-watcord$V2)**2 + (watcord$V3[i]-watcord$V3)**2)
+  distances[i] <- min(distances2[which(distances2!=0)])
+}
+
+paste(mean(distances), "Å")
